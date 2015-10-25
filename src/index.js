c959a5c516070327eaabbc570b21bd07e323d5f9
tomascorralcasas <amischol@gmail.com>
2015-10-26 00:50:31 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var path = require('path');
+var audiosprite = require('audiosprite');
+var clips = [];
+
+module.exports = function (codeFilePath, songPath, callback) {
+  var data = fs.readFileSync(codeFilePath, 'utf8');
+  var chars = data.replace(/\r/g, '').replace(/\n/g, '').split('');
+  var opts = { output: songPath, gap: -2, export: 'mp3'};
+  chars.forEach(function (char){
+    var file;
+    var _path;
+    var stats;
+    try{
+      file = char;
+      _path = path.join(__dirname,  '..', 'sounds', file + '.mp3');
+      stats = fs.statSync(_path);
+      if(stats.isFile()){
+        clips.push(_path);
+      }
+    } catch(er) {
+
+    }
+  });
+
+  audiosprite(clips, opts, function(err, obj) {
+    if (err) {
+      callback(err);
+      return console.error(err);
+    }
+    callback(null, obj);
+    console.log('Done');
+  });
+};
+
---
+++ test/index_test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var fs = require('fs');
+var code2mp3 = require('../src');
+var cwd = process.cwd();
+
+exports.code2mp3 = {
+  'test that it should return the same song of a single line of code': function (test) {
+    code2mp3(cwd + '/test/fixtures/a.js', 'test/result/song_1', function () {
+      var expected = fs.readFileSync(__dirname + '/expected/song_1.mp3', 'utf8');
+      var result = fs.readFileSync(__dirname + '/result/song_1.mp3', 'utf8');
+      test.equal(result, expected);
+      test.done();
+    });
+  },
+  'test that it should return the same song of a code file': function (test) {
+    code2mp3(cwd + '/test/fixtures/file.js', 'test/result/song_2', function () {
+      var expected = fs.readFileSync(__dirname + '/expected/song_2.mp3', 'utf8');
+      var result = fs.readFileSync(__dirname + '/result/song_2.mp3', 'utf8');
+      test.equal(result, expected);
+      test.done();
+    });
+  }
+};
