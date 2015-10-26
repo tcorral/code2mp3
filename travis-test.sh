1aea7b3e2cac068eeb47df6f46c29dd5f6b5a202
tomascorralcasas <amischol@gmail.com>
2015-10-26 04:07:08 +0100

    Test install ffmpeg

--- travis-test.sh
+++ travis-test.sh
@@ -6,21 +6,7 @@ set -e
 # Install dependencies
 
 echo travis_fold:start:Dependencies
-if [ "$(uname)" = "Linux" ]; then
-	# Linux
-	sudo apt-get update
-	sudo apt-get -y install wget tar bzip2 flvtool2
-
-	wget http://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz
-	tar xf ffmpeg-git-64bit-static.tar.xz
-
-	sudo cp ffmpeg-git-*-static/{ffmpeg,ffprobe,ffserver} /usr/bin
-	sudo cp ffmpeg-git-*-static/{ffmpeg,ffprobe} $(pwd)
-
-	export ALT_FFMPEG_PATH=$(pwd)/ffmpeg
-	export ALT_FFPROBE_PATH=$(pwd)/ffprobe
-else
-	# OSX
+    # OSX
 	brew update
 	brew install ffmpeg
 	brew install flvmeta
@@ -34,7 +20,6 @@ else
 	export ALT_FFMPEG_PATH=$HOME/ffmpeg
 	cp $(which ffprobe) ~/ffprobe
 	export ALT_FFPROBE_PATH=$HOME/ffprobe
-fi
 echo travis_fold:end:Dependencies
 
 # Install nvm if needed
@@ -57,6 +42,7 @@ echo travis_fold:end:Versions
 
 # Install dependencies
 echo travis_fold:start:npm-install
+npm install -g grunt-cli
 npm install
 echo travis_fold:end:npm-install
 
