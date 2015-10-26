6c6818e747a6d951a73199bd31bbeca6c76811cd
tomascorralcasas <amischol@gmail.com>
2015-10-26 01:16:49 +0100

    Test install ffmpeg

--- install-ffmpeg.sh
+++ install-ffmpeg.sh
@@ -1,106 +1,74 @@
 #!/bin/bash
 
-#This script will install and configure the following modules for you
-# ffmpeg 0.7.11, mencoder, mplayer, mp4box2 and more libs
+# Bash script to install latest version of ffmpeg and its dependencies on Ubuntu 12.04 or 14.04
+# Inspired from https://gist.github.com/faleev/3435377
 
-### Made by Shubham Mathur (itseasy21)
+# Remove any existing packages:
+sudo apt-get -y remove ffmpeg x264 libav-tools libvpx-dev libx264-dev
 
-#-Colors :D
-RED='\033[01;31m'
-GREEN='\033[01;32m'
-RESET='\033[0m'
+# Get the dependencies (Ubuntu Server or headless users):
+sudo apt-get update
+sudo apt-get -y install build-essential checkinstall git libfaac-dev libgpac-dev \
+  libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev librtmp-dev libtheora-dev \
+    libvorbis-dev pkg-config texi2html yasm zlib1g-dev
 
-#--Clearing Your Screen
-clear
+# Install x264
+sudo apt-get -y install libx264-dev
+cd
+git clone --depth 1 git://git.videolan.org/x264
+cd x264
+./configure --enable-static
+make
+sudo checkinstall --pkgname=x264 --pkgversion="3:$(./version.sh | \
+  awk -F'[" ]' '/POINT/{print $4"+git"$5}')" --backup=no --deldoc=yes \
+    --fstrans=no --default
 
-echo -e "$GREEN----------------------------------------$RESET"
-echo -e "     $RED W3TooLS FFMpeg Installer $RESET"
-echo -e "              Version 0.1.1            "
-echo -e "     http://www.w3tool.blogspot.in/  "
-echo -e "$GREEN----------------------------------------$RESET"
-
-#-Yum Check
-echo -ne "Checking for Yum .."
-if [ -e  "/etc/yum.conf" ]; then
-	echo -e "[ $GREEN Found $RESET ]"
-else
-	echo -e "[ $RED Not Found $RESET ]"
-	exit
-fi
-
-#--Making Sure we have required libs
-cd /root
-yum install vim wget sed rpm gcc -y
-
-#--Libs Check
-echo " "
-echo -ne "Checking for SED .."
-if [ -e  "/bin/sed" ]; then
-	echo -e "[ $GREEN Found $RESET ]"
-else
-	echo -e "[ $RED Not Found $RESET ]"
-	exit
-fi
-
-#--checking old ffmpeg installation
-echo -ne "Searching for Previous FFMpeg Installation .."
-if [ -e  "/usr/bin/ffmpeg" ]; then
-	echo -e "[ $RED FFMpeg Found.\n Exiting Install. $RESET ]"
-	exit
-else
-	echo -e "[ $GREEN FFMpeg Not Found, Proceeding ... $RESET ]"
-fi
-
-#--Adding repo
-cd /root
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-wget --no-check-certificate https://raw.githubusercontent.com/itseasy21/auto-ffmpeg/master/auto-ffmpeg.repo -O /etc/yum.repos.d/auto-ffmpeg.repo
-
-#getting os info
-os=$( uname -i )
-
-#installing ffmpeg, mplayer, mp4box2, and other libs
-if [ "$os" = "x86_64" ]
-then
-   yum install ffmpeg mplayer gpac gpac-libs ffmpeg-devel --exclude "*.i386" -y
-else
-   yum install ffmpeg mplayer gpac gpac-libs ffmpeg-devel -y
-fi
-
-#installing ffmpeg-php
-cd /root
-wget http://downloads.sourceforge.net/ffmpeg-php/ffmpeg-php-0.6.0.tbz2
-tar xjf ffmpeg-php-0.6.0.tbz2
-cd ffmpeg-php-0.6.0
-sed -i 's/PIX_FMT_RGBA32/PIX_FMT_RGB32/g' ffmpeg_frame.c
-#3 new fix for centos 6.5
-sed -i 's/list_entry *le/zend_rsrc_list_entry *le/g' ffmpeg_movie.c
-sed -i 's/list_entry new_le/zend_rsrc_list_entry new_le/g' ffmpeg_movie.c
-sed -i 's/sizeof(list_entry)/sizeof(zend_rsrc_list_entry)/g' ffmpeg_movie.c
-phpize
+# Install AAC audio decoder
+cd
+wget http://downloads.sourceforge.net/opencore-amr/fdk-aac-0.1.0.tar.gz
+tar xzvf fdk-aac-0.1.0.tar.gz
+cd fdk-aac-0.1.0
 ./configure
 make
-make install
+sudo checkinstall --pkgname=fdk-aac --pkgversion="0.1.0" --backup=no \
+  --deldoc=yes --fstrans=no --default
 
-#finding php.ini and adding ffmpeg in it
-phpini=$( (php -i | grep "Loaded Configuration File") | sed -e 's/Loaded Configuration File => //g' )
-echo "extension=\"ffmpeg.so\"" >> $phpini
+# Install VP8 video encoder and decoder.
+cd
+git clone --depth 1 https://chromium.googlesource.com/webm/libvpx
+cd libvpx
+./configure
+make
+sudo checkinstall --pkgname=libvpx --pkgversion="1:$(date +%Y%m%d%H%M)-git" --backup=no \
+  --deldoc=yes --fstrans=no --default
 
-#restarting http
-/etc/init.d/httpd restart
 
-#finished
-echo -e "$GREEN ---------------------------------------------------$RESET"
-echo -e "$GREEN      FFMpeg Installation Complete       $RESET"
-echo -e "        Below are the paths of various libs  "
-echo -e "
-    ffmpeg:$GREEN /usr/bin/ffmpeg $RESET
-    mplayer:$GREEN /usr/bin/mplayer $RESET
-    mencoder:$GREEN /usr/bin/mencoder $RESET
-    MP4Box:$GREEN /usr/bin/MP4Box $RESET"
-echo -e ""
-echo -e "If ffmpeg failed to install or you faced any bugs feel free to post them with install log at $GREEN https://github.com/itseasy21/ $RESET"
-echo -e ""
-echo -e "Thanks for using our FFMpeg Installer for CentOS"
-echo -e "For more stay tuned at our blog : $GREEN http://www.w3tool.blogspot.in/ $RESET"
-echo -e "$GREEN---------------------------------------------------$RESET"
+# Add lavf support to x264
+# This allows x264 to accept just about any input that FFmpeg can handle and is useful if you want to use x264 directly. See a more detailed explanation of what this means.
+cd ~/x264
+make distclean
+./configure --enable-static
+make
+sudo checkinstall --pkgname=x264 --pkgversion="3:$(./version.sh | \
+  awk -F'[" ]' '/POINT/{print $4"+git"$5}')" --backup=no --deldoc=yes \
+  --fstrans=no --default
+
+# Installing FFmpeg
+cd
+git clone --depth 1 git://source.ffmpeg.org/ffmpeg
+cd ffmpeg
+./configure --enable-gpl --enable-libfaac --enable-libmp3lame --enable-libopencore-amrnb \
+  --enable-libopencore-amrwb --enable-librtmp --enable-libtheora --enable-libvorbis \
+    --enable-libvpx --enable-libx264 --enable-nonfree --enable-version3
+make
+sudo checkinstall --pkgname=ffmpeg --pkgversion="5:$(date +%Y%m%d%H%M)-git" --backup=no \
+  --deldoc=yes --fstrans=no --default
+  hash x264 ffmpeg ffplay ffprobe
+
+# Optional: install qt-faststart
+# This is a useful tool if you're showing your H.264 in MP4 videos on the web. It relocates some data in the video to allow playback to begin before the file is completely downloaded. Usage: qt-faststart input.mp4 output.mp4.
+cd ~/ffmpeg
+make tools/qt-faststart
+sudo checkinstall --pkgname=qt-faststart --pkgversion="$(date +%Y%m%d%H%M)-git" --backup=no \
+  --deldoc=yes --fstrans=no --default install -Dm755 tools/qt-faststart \
+  /usr/local/bin/qt-faststart
