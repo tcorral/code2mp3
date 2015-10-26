fa62bd032af0b342d653ab5fbe16c311ac60d3e0
tomascorralcasas <amischol@gmail.com>
2015-10-26 09:40:16 +0100

    Fix tests

--- test/index_test.js
+++ test/index_test.js
@@ -2,19 +2,20 @@
 
 var fs = require('fs');
 var code2mp3 = require('../src');
-var cwd = process.cwd();
 
 exports.code2mp3 = {
   'test that it should return the same song of a single line of code': function (test) {
-    code2mp3(cwd + '/test/fixtures/a.js', cwd + '/test/result/song_1', function () {
-      var result = fs.readFileSync(cwd + '/test/result/song_1.mp3', 'utf8').length;
+    code2mp3(__dirname + '/fixtures/a.js', __dirname + '/result/song_1', function () {
+      var result = fs.readFileSync(__dirname + '/result/song_1.mp3', 'utf8').length;
+      console.log(result);
       test.ok(result > 0);
       test.done();
     });
   },
   'test that it should return the same song of a code file': function (test) {
-    code2mp3(cwd + '/test/fixtures/file.js', cwd + '/test/result/song_2', function () {
-      var result = fs.readFileSync(cwd + '/test/result/song_2.mp3', 'utf8').length;
+    code2mp3(__dirname + '/fixtures/file.js', __dirname + '/result/song_2', function () {
+      var result = fs.readFileSync(__dirname + '/result/song_2.mp3', 'utf8').length;
+      console.log(result);
       test.ok(result > 0);
       test.done();
     });
