c9df56bfea571d868d946e840e74c20da42a3d78
tomascorralcasas <amischol@gmail.com>
2015-10-26 10:02:40 +0100

    Fix tests

--- test/index_test.js
+++ test/index_test.js
@@ -5,7 +5,8 @@ var code2mp3 = require('../src');
 
 exports.code2mp3 = {
   'test that it should return the same song of a single line of code': function (test) {
-    code2mp3(__dirname + '/fixtures/a.js', __dirname + '/result/song_1', function () {
+    code2mp3(__dirname + '/fixtures/a.js', __dirname + '/result/song_1', function (err, obj) {
+      console.log(obj);
       var result = fs.readFileSync(__dirname + '/result/song_1.mp3', 'utf8').length;
       console.log(result);
       test.ok(result > 0);
@@ -13,7 +14,8 @@ exports.code2mp3 = {
     });
   },
   'test that it should return the same song of a code file': function (test) {
-    code2mp3(__dirname + '/fixtures/file.js', __dirname + '/result/song_2', function () {
+    code2mp3(__dirname + '/fixtures/file.js', __dirname + '/result/song_2', function (err, obj) {
+      console.log(obj);
       var result = fs.readFileSync(__dirname + '/result/song_2.mp3', 'utf8').length;
       console.log(result);
       test.ok(result > 0);
